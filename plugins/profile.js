66f4138b5428e02978762f9a9a71646454f82895
aingang028 <82096519+aingang028@users.noreply.github.com>
2021-04-14 10:28:34 +0700

    Add files via upload

--- plugins/profile.js
+++ plugins/profile.js
@@ -1,5 +1,4 @@
 let PhoneNumber = require('awesome-phonenumber')
-let levelling = require('../lib/levelling')
 let handler = async (m, { conn }) => {
   let pp = './src/avatar_contact.png'
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
@@ -8,18 +7,17 @@ let handler = async (m, { conn }) => {
   } catch (e) {
 
   } finally {
-    let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
-    let { name, limit, exp, lastclaim, registered, regTime, age, level } = global.DATABASE.data.users[m.sender]
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
-    let username = conn.getName(who)
+    let name = conn.getName(who)
+    let about = (await conn.getStatus(who)).status
+    let { limit, exp, lastclaim } = global.DATABASE.data.users[m.sender]
     let str = `
-Name: ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})${about ? '\nAbout: ' + about : ''}
+Name: ${name} (@${who.replace(/@.+/, '')})
+About: ${about}
 Number: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-Link: https://wa.me/${who.split`@`[0]}${registered ? '\nAge: ' + age : ''}
-XP: TOTAL ${exp} (${exp - min} / ${xp}) [${max - exp} left to levelup]
-Level: ${level}
+Link: https://wa.me/${who.split`@`[0]}
+XP: ${exp}
 Limit: ${limit}
-Registered: ${registered ? 'Yes (' + new Date(regTime) + ')': 'No'}${lastclaim > 0 ? '\nLast Claim: ' + new Date(lastclaim) : ''}
+Last Claim: ${new Date(lastclaim)}
 `.trim()
     let mentionedJid = [who]
     conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})
--- plugins/register.js
+++ plugins/register.js
@@ -2,24 +2,34 @@ const { createHash } = require('crypto')
 let Reg = /(.*)([.|])([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.DATABASE._data.users[m.sender]
-  if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `Foemat salah\n*${usedPrefix}daftar <nama>.umur>*`
+  if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg ( SN|YOUR NUMBER )`
+  if (!Reg.test(text)) throw `Format salah!\n*Harap daftar dengan Benar!\nDengan Ketik *${usedPrefix}daftar <nama>.umur>*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
-  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  if (!name) throw 'Nama tidak boleh kosong cukk... gak bisa :v\nHarus diisi dengan Benar dan tidak menipu *_OWNER/BOT!_*'
+  if (!age) throw 'Umur tidak boleh kosong dan diisi dengan angka\nNote: Tidak menipu Umur Kamu di *_OWNER/BOT_*'
   user.name = name
   user.age = parseInt(age)
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-Daftar berhasil!
+Anda sudah Berhasil Terdaftar dan,
+User kamu Dibawah 👇
 
-╭─「 Info 」
-│ Nama: ${name}
-│ Umur: ${age}thn
-│ SN: ${sn}
-╰────
+           ╔───────────────┐
+           │ ABOUT YOU WITH BOT │
+           └───────────────╝
+           
+┏━━⊱ 🧚🏿‍♂️❰ *INFO* ❱ 🧚🏿‍♂️⊰━━☬
+┣𖥻ꦼꦽ➳ Nama: ${name}
+┣𖥻ꦼꦽ➳ Umur: ${age}Tahun
+┣𖥻ꦼꦽ➳SN: ${sn}
+┣𖥻ꦼꦽ➳Command To Help *_#menu_*
+│
+┣𖥻ꦼꦽ➳Follow Instagram: *@aingang028*
+┣𖥻ꦼꦽ➳ YouTube: *Aing Ang*
+│
+┗━━⊱  ⸨ *TERIMAKASIH* ⸩  ⊰━━━☬
 `.trim())
 }
 handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
